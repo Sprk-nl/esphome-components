a4450a2f7158fd8b5c238ef8263ae345349407d7
Sprk <gaston.bougie@gmail.com>
2025-08-31 23:24:49 +0200

    Update wifi_csi.cpp
    changed 
    time(&now_t);
    ::time(&now_t);

--- components/wifi_csi/wifi_csi.cpp
+++ components/wifi_csi/wifi_csi.cpp
@@ -88,7 +88,7 @@ void esphome::wifi_csi::CsiSensor::update() {
         // log every 5 seconds
         static time_t last_t;
         time_t now_t;
-        time(&now_t);
+        ::time(&now_t);
         if (difftime(now_t, last_t) > 5.0) {
             ESP_LOGD(TAG, "idx: %d, cnt: %d: avg: %.1f, current: %d, sensitvity: %d, motion: %d", idx, cnt, avgerageRssi, currentRssi, m_sensitivity, motion);
             last_t = now_t;
